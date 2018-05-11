69cdfa08fce1db5bc63f76b61c162aee20591a0a
stefan.r <stefan.r@551886.no-reply.drupal.org>
2018-05-11 22:50:08 +0100

    add grid constraint, turn left/right/forward

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const createNamedTuple = require('named-tuple')
+const createNamedTuple = require('named-tuple');
 
-const Point = createNamedTuple('Point', 'x', 'y')
+const Point = createNamedTuple('Point', 'x', 'y');
+const Grid = createNamedTuple('Grid', 'xSize', 'ySize');
+const Shift = createNamedTuple('Shift', 'shiftX', 'shiftY');
 
 class Robot {
 
   /**
    * @param number x - x coord
    * @param number y - y coord
+   * @param number maxX - max x coord
+   * @param number maxY - max y coord
    * @param char orientation - N, E, S, or W
    */
-  constructor(x, y, orientation) {
+  constructor(x, y, orientation, grid) {
+    this.grid = grid;
     this.orientations = {
       'N': 0,
       'E': 1,
       'S': 2,
       'W': 3
     };
-    this.orientationChars = ['N', 'E', 'S', 'W']
+    this.orientationChars = ['N', 'E', 'S', 'W'];
+    this.shifts = [new Shift(0,1), new Shift(1,0), new Shift(0,-1), new Shift(-1,0)];
     this.position = new Point(x, y);
     this.orientation = this.orientations[orientation];
   }
@@ -25,14 +31,14 @@ class Robot {
    * Turns the robot to the left.
    */
   turnLeft() {
-    this.orientation = (this.orientation + 1) & 3;
+    this.orientation = (this.orientation - 1) & 3;
   }
 
   /**
    * Turns the robot to the right.
    */
   turnRight() {
-    this.orientation = (this.orientation - 1) & 3;
+    this.orientation = (this.orientation + 1) & 3;
   }
 
   /**
@@ -41,9 +47,18 @@ class Robot {
   orientationChar() {
     return this.orientationChars[this.orientation];
   }
+
+  moveForward() {
+    const nextPosition = new Point(this.position.x + this.shifts[this.orientation].shiftX, this.position.y + this.shifts[this.orientation].shiftY);
+    // prevent robot moving off the grid
+    if ((nextPosition.x >= 0) && (nextPosition.x < this.grid.xSize) && (nextPosition.y >= 0) && (nextPosition.y < this.grid.ySize)) {
+      this.position = nextPosition;
+    }
+  }
 }
 
 module.exports = {
   Robot: Robot,
-  Point: Point
+  Point: Point,
+  Grid: Grid
 }
--- test.js
+++ test.js
@@ -3,10 +3,71 @@ const expect = require('chai').expect;
 
 describe('Robot', () => {
   it('should return the right orientation & position', () => { 
-    const robot = new app.Robot(0,0, 'N')
+    const grid = new app.Grid(3, 3)
+    const robot = new app.Robot(0,0, 'N', grid)
     expect(robot.orientation).to.equal(0)
     expect(robot.orientationChar()).to.equal('N')
     expect(robot.position).to.eql(new app.Point(0,0))
+  });
+
+  it('should turn left / right', () => { 
+    const grid = new app.Grid(3, 3)
+    const robot = new app.Robot(0,0, 'N', grid)
+    expect(robot.orientation).to.equal(0)
+    expect(robot.orientationChar()).to.equal('N')
+    robot.turnLeft();
+    expect(robot.orientation).to.equal(3)
+    expect(robot.orientationChar()).to.equal('W')
+    robot.turnLeft();
+    expect(robot.orientation).to.equal(2)
+    expect(robot.orientationChar()).to.equal('S')
+    robot.turnLeft();
+    expect(robot.orientation).to.equal(1)
+    expect(robot.orientationChar()).to.equal('E')
+    robot.turnLeft();
+    expect(robot.orientation).to.equal(0)
+    expect(robot.orientationChar()).to.equal('N')
+    robot.turnRight();
+    expect(robot.orientation).to.equal(1)
+    expect(robot.orientationChar()).to.equal('E')
+    robot.turnRight();
+    expect(robot.orientation).to.equal(2)
+    expect(robot.orientationChar()).to.equal('S')
+    robot.turnRight();
+    expect(robot.orientation).to.equal(3)
+    expect(robot.orientationChar()).to.equal('W')
+    robot.turnRight();
+    expect(robot.orientation).to.equal(0)
+    expect(robot.orientationChar()).to.equal('N')
+  });
+
+  it('should move forward', () => { 
+    const grid = new app.Grid(3, 3)
+    const robot = new app.Robot(0,0, 'N', grid)
+    expect(robot.position).to.eql(new app.Point(0,0))
+    expect(robot.orientation).to.equal(0)
+    expect(robot.orientationChar()).to.equal('N')
+    robot.moveForward();
+    expect(robot.orientation).to.equal(0)
+    expect(robot.orientationChar()).to.equal('N')
+    expect(robot.position).to.eql(new app.Point(0,1))
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(0,2))
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(0,2))
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(0,2))
+    robot.turnLeft();
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(0,2))
+    robot.turnRight();
+    robot.turnRight();
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(1,2))
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(2,2))
+    robot.moveForward();
+    expect(robot.position).to.eql(new app.Point(2,2))
   });
 });
 
